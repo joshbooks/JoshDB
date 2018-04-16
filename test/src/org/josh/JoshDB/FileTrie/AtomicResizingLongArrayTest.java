8479202fcc3bf0c10f3799e2d428bfe7a7f7ebaf
Josh <joshbooks@gmail.com>
2018-04-15 23:35:58 -0700

    Add multithreaded test

--- test/src/org/josh/JoshDB/FileTrie/AtomicResizingLongArrayTest.java
+++ test/src/org/josh/JoshDB/FileTrie/AtomicResizingLongArrayTest.java
@@ -22,4 +22,42 @@ public class AtomicResizingLongArrayTest
             assert ByteBuffer.wrap(array.get(i, 8)).getLong() == i;
         }
     }
+
+    @Test
+    public void testAddUpMultiThreaded() throws InterruptedException
+    {
+        AtomicResizingLongArray array = new AtomicResizingLongArray();
+        Thread[] testThreads = new Thread[0x100];
+
+        for (int i = 0; i < 0x100; i += 8)
+        {
+            final int finalI = i;
+            testThreads[i] =
+            new Thread(() ->
+            {
+                try
+                {
+                    array.set(finalI, ByteBuffer.allocate(8).putLong(finalI).array());
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            );
+
+            testThreads[i].start();
+        }
+
+        for (int i = 0; i < 0x100; i += 8)
+        {
+            testThreads[i].join();
+        }
+
+        for (int i = 0; i < 0x100; i += 8)
+        {
+
+            assert ByteBuffer.wrap(array.get(i, 8)).getLong() == i;
+        }
+    }
 }
